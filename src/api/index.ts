c5da01fd324f41dad77153fa3bd4120f06e03111
linrunxinnn <lrxczy03140125@gmail.com>
2025-08-16 15:22:22 +0800

    fix: 删了两个输出

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,8 +10,8 @@ import { useNavigate } from "react-router-dom";
 // 创建axios实例
 const apiClient: AxiosInstance = axios.create({
   // baseURL: "http://47.113.224.195:32406",
-  // baseURL: "http://192.168.1.161:8080",
-  baseURL: "http://47.113.224.195:30422/api",
+  baseURL: "http://47.113.224.195:32400",
+  // baseURL: "http://47.113.224.195:30422/api",
   timeout: 100000,
   headers: {
     "Content-Type": "application/json",
